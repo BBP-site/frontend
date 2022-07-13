cba9c5181fd6c79ae61233a56fdb16de0be289b1
Gleb Bazanov <baznow.dev@gmail.com>
2022-07-13 03:08:07 +0300

    #27 add carousel component

--- src/components/common/Block/index.tsx
+++ src/components/common/Block/index.tsx
@@ -2,12 +2,12 @@ import { ReactNode, FC } from 'react';
 import { CSSObject } from '@emotion/react';
 import { colors, shadows } from '@scripts/theme';
 
-export interface BlockProps {
+export interface IBlockProps {
   css?: CSSObject;
   children?: ReactNode;
 }
 
-const Block: FC<BlockProps> = ({ css, children, ...props }) => (
+const Block: FC<IBlockProps> = ({ css, children, ...props }) => (
   <div
     css={{ padding: '24px', backgroundColor: colors?.white, borderRadius: '8px', ...shadows.around, ...css }}
     {...props}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,17 @@
 import Link from 'next/link';
 
-import { links, typography, pageWrap } from '@scripts/theme';
+import { links, pageWrap } from '@scripts/theme';
 
 import Button from '@components/common/Button';
+import Carousel from '@components/common/Carousel';
+import Block from '@components/common/Block';
 
 import ArrowIcon from '@icons/arrow.svg';
 
 const Home = () => (
   <main css={{ marginBottom: '32px' }}>
-    <h1 css={{ ...typography.h1 }}>Заголовок главной страницы</h1>
-    <h4 css={typography.h4}>Заголовок 4 уровня</h4>
+    <h1>Заголовок главной страницы</h1>
+    <h4>Заголовок 4 уровня</h4>
     <div css={pageWrap}>
       <Button css={{ marginRight: '32px' }}>Hi! Im new button</Button>
       <Link href="/contacts" passHref>
@@ -17,6 +19,25 @@ const Home = () => (
           Перейти к контактам <ArrowIcon css={{ marginLeft: '10px', transform: 'rotate(90deg)' }} />
         </a>
       </Link>
+      <p>Параграф</p>
+      <p>
+        <i>Параграф2</i>
+      </p>
+      <Button css={{ marginBottom: '32px' }}>Another button</Button>
+      <Carousel
+        slidesPerView={1}
+        spaceBetween={16}
+        breakpoints={{
+          900: {
+            slidesPerView: 2,
+            spaceBetween: 16,
+          },
+        }}
+      >
+        {[1, 2, 3, 4, 5, 6, 7].map(i => (
+          <Block key={i} css={{ maxWidth: '628px', height: '239px' }}>{`Slide ${i}`}</Block>
+        ))}
+      </Carousel>
     </div>
   </main>
 );
