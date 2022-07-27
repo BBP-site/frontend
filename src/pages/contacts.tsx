d84a91144a68d1617b676ab97e1b509e92c36070
Gleb Bazanov <baznow.dev@gmail.com>
2022-07-27 23:49:48 +0300

    #28 expand blocks with bgclr

--- src/pages/contacts.tsx
+++ src/pages/contacts.tsx
@@ -25,69 +25,67 @@ const Contacts = () => {
 
       <div
         css={{
-          ...pageWrap,
-          paddingTop: '64px',
-          paddingBottom: '64px',
-          ...position.spaceBetween,
           backgroundColor: colors.gray100,
         }}
       >
-        <div css={{ display: 'flex' }}>
-          <div
-            css={{
-              ...position.center,
-              backgroundColor: colors.blueLight,
-              borderRadius: '50%',
-              width: '52px',
-              height: '52px',
-              marginRight: '24px',
-            }}
-          >
-            <Image src={pinURL} width={32} height={32} />
+        <div css={{ ...pageWrap, paddingTop: '64px', paddingBottom: '64px', ...position.spaceBetween }}>
+          <div css={{ display: 'flex' }}>
+            <div
+              css={{
+                ...position.center,
+                backgroundColor: colors.blueLight,
+                borderRadius: '50%',
+                width: '52px',
+                height: '52px',
+                marginRight: '24px',
+              }}
+            >
+              <Image src={pinURL} width={32} height={32} />
+            </div>
+            <div css={{ marginRight: '32px' }}>
+              <p css={{ ...typography.h6, marginBottom: '8px' }}>Адрес</p>
+              <p css={{ ...typography.txt, color: colors.gray700 }}>{contactsData.address}</p>
+            </div>
           </div>
-          <div css={{ marginRight: '32px' }}>
-            <p css={{ ...typography.h6, marginBottom: '8px' }}>Адрес</p>
-            <p css={{ ...typography.txt, color: colors.gray700 }}>{contactsData.address}</p>
+          <div css={{ display: 'flex' }}>
+            <div
+              css={{
+                ...position.center,
+                backgroundColor: colors.blueLight,
+                borderRadius: '50%',
+                width: '52px',
+                height: '52px',
+                marginRight: '24px',
+              }}
+            >
+              <Image src={phoneCallURL} width={32} height={32} />
+            </div>
+            <div css={{ marginRight: '32px' }}>
+              <p css={{ ...typography.h6, marginBottom: '8px' }}>Телефон</p>
+              {contactsData.phones.map(phone => (
+                <p key={phone.desc} css={{ ...typography.txt, color: colors.gray700, marginBottom: 0 }}>
+                  {phone.number} <span css={{ whiteSpace: 'nowrap' }}>{phone.desc}</span>
+                </p>
+              ))}
+            </div>
           </div>
-        </div>
-        <div css={{ display: 'flex' }}>
-          <div
-            css={{
-              ...position.center,
-              backgroundColor: colors.blueLight,
-              borderRadius: '50%',
-              width: '52px',
-              height: '52px',
-              marginRight: '24px',
-            }}
-          >
-            <Image src={phoneCallURL} width={32} height={32} />
-          </div>
-          <div css={{ marginRight: '32px' }}>
-            <p css={{ ...typography.h6, marginBottom: '8px' }}>Телефон</p>
-            {contactsData.phones.map(phone => (
-              <p key={phone.desc} css={{ ...typography.txt, color: colors.gray700, marginBottom: 0 }}>
-                {phone.number} <span css={{ whiteSpace: 'nowrap' }}>{phone.desc}</span>
-              </p>
-            ))}
-          </div>
-        </div>
-        <div css={{ display: 'flex' }}>
-          <div
-            css={{
-              ...position.center,
-              backgroundColor: colors.blueLight,
-              borderRadius: '50%',
-              width: '52px',
-              height: '52px',
-              marginRight: '24px',
-            }}
-          >
-            <Image src={mailURL} width={32} height={32} />
-          </div>
-          <div>
-            <p css={{ ...typography.h6, marginBottom: '8px' }}>E-mail</p>
-            <p css={{ ...typography.txt, color: colors.gray700 }}>{contactsData.email}</p>
+          <div css={{ display: 'flex' }}>
+            <div
+              css={{
+                ...position.center,
+                backgroundColor: colors.blueLight,
+                borderRadius: '50%',
+                width: '52px',
+                height: '52px',
+                marginRight: '24px',
+              }}
+            >
+              <Image src={mailURL} width={32} height={32} />
+            </div>
+            <div>
+              <p css={{ ...typography.h6, marginBottom: '8px' }}>E-mail</p>
+              <p css={{ ...typography.txt, color: colors.gray700 }}>{contactsData.email}</p>
+            </div>
           </div>
         </div>
       </div>
