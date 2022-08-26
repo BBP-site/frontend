95434ce4e1010ae451a5d954606636cd877746c5
igor <igordevm@gmail.com>
2022-08-26 15:15:26 +0300

    add collegium text

--- src/mocks/achievements.ts
+++ src/mocks/achievements.ts
@@ -1,6 +1,4 @@
 import {StaticImageData} from "next/image";
-
-import bestURL from '@images/best.png';
 import globalURL from '@images/global.webp';
 import rating1URL from "@images/rating1.webp";
 import rating2URL from "@images/rating2.webp";
@@ -31,11 +29,11 @@ export const achievements: IAchievements[] = [
         id: '4',
         img: globalURL,
     },
-    {
-        id: '6',
-        img: bestURL,
-        isHaveSize: true,
-        height: '200px',
-        width: '200px',
-    },
+    // {
+    //     id: '6',
+    //     img: bestURL,
+    //     isHaveSize: true,
+    //     height: '200px',
+    //     width: '200px',
+    // },
 ]
